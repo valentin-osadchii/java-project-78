1ec0c8d2e83622d3e395321853ae606fe3523d96
VOsadchii <909404@gmail.com>
2025-12-20 14:18:12 +0300

    configure sonar and checkstyle

---
+++ app/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    id("java")
+    id("checkstyle")
+    id("org.sonarqube") version "7.2.2.6593"
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+
+tasks.check {
+    dependsOn(tasks.checkstyleMain, tasks.checkstyleTest)
+}
+
+tasks.build {
+    dependsOn(tasks.check)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+
+sonar {
+    properties {
+        property("sonar.projectKey", "valentin-osadchii_java-project-78")
+        property("sonar.organization", "valentin-osadchii")
+    }
+}
