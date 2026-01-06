fd6e86c66076c0f2a92d0b2f9c5dab1642b5c33b
VOsadchii <909404@gmail.com>
2026-01-06 22:43:03 +0300

    add javadoc to baseclass and method to be overridden

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -1,12 +1,30 @@
 package hexlet.code.schemas;
 
+/**
+ * Abstract base class for all validation schemas.
+ * Provides common functionality such as the 'required' flag.
+ *
+ * @param <T> the type of value this schema validates
+ */
 public abstract class BaseSchema<T> {
     protected boolean required = false;
 
+    /**
+     * Marks the schema as required (null values are not allowed).
+     * Returns this schema for method chaining.
+     *
+     * @return this instance
+     */
     public BaseSchema<T> required() {
         this.required = true;
         return this;
     }
 
+    /**
+     * Validates the given value against the schema rules.
+     *
+     * @param value the value to validate (may be null)
+     * @return true if valid, false otherwise
+     */
     public abstract boolean isValid(Object value);
 }
